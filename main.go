38ba371f137e6d8cc2d92d64a8f91396848648ed
Tregan Emmett <tregan@tregan.me>
2020-02-28 11:02:44 -0700

    i forgot what this does ¯\_(ツ)_/¯

--- main.go
+++ main.go
@@ -2,44 +2,98 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
+	"strings"
+	"sync"
 
-	"github.com/haccer/available"
+	"github.com/domainr/whois"
 )
 
-func main() {
-	name := os.Args[1]
+func toLetters(num int) string {
+	power := num / 26
+	mod := num % 26
 
-	fmt.Print(".com\t\t")
+	var output string
 
-	available := available.Domain(name + ".com")
-
-	if available {
-		fmt.Println("🔥")
+	if mod > 0 {
+		output = string('A' - 1 + mod)
 	} else {
-		fmt.Println("👎")
+		power -= 1
+		output = string('Z')
 	}
 
-	names := []string{"Github", "NPM", "Slack", "Reddit", "Twitter", "Insta", "FB"}
+	var returnValue string
 
-	urls := []string{
-		"https://github.com/" + name,
-		"https://www.npmjs.com/org/" + name,
-		"https://" + name + ".slack.com/",
-		"https://www.reddit.com/r/" + name,
-		"https://twitter.com/" + name,
-		"https://www.instagram.com/" + name,
-		"https://www.facebook.com/" + name}
-
-	for i, url := range urls {
-		fmt.Print(names[i] + "\t\t")
-		resp, _ := http.Get(url)
-
-		if resp.StatusCode == 200 {
-			fmt.Println("👎")
-		} else {
-			fmt.Println("🔥")
-		}
+	if power > 0 {
+		returnValue = toLetters(power) + output
+	} else {
+		returnValue = output
 	}
+
+	return strings.ToLower(returnValue)
+}
+
+func appendFile(value string) {
+	file, err := os.OpenFile("names.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("\n-------------------------------")
+		fmt.Println("Write to file failed.")
+		fmt.Println(value)
+		fmt.Println("-------------------------------\n")
+	}
+	defer file.Close()
+	if _, err := file.WriteString(value + "\n"); err != nil {
+		fmt.Println("\n-------------------------------")
+		fmt.Println("Write to file failed.")
+		fmt.Println(value)
+		fmt.Println("-------------------------------\n")
+	}
+}
+
+func nameAvailable(name string) bool {
+	request, err := whois.NewRequest(name)
+
+	if err != nil {
+		fmt.Println("\n-------------------------------")
+		fmt.Println("Request failed.")
+		fmt.Println(name)
+		fmt.Println("-------------------------------\n")
+		return false
+	}
+
+	response, err := whois.DefaultClient.Fetch(request)
+
+	if err != nil {
+		fmt.Println("\n-------------------------------")
+		fmt.Println("Response failed.")
+		fmt.Println(name)
+		fmt.Println("-------------------------------\n")
+		return false
+	}
+
+	str := response.String()
+	return strings.HasPrefix(str, "")
+}
+
+func main() {
+	max := 100
+	min := 27
+	var wg sync.WaitGroup
+
+	wg.Add(max - min)
+
+	for i := min; i < max; i++ {
+		go func(i int) {
+			defer wg.Done()
+			host := toLetters(i)
+			fmt.Println(i, host)
+			domain := host + ".io"
+			available := nameAvailable(domain)
+			if available {
+				appendFile(domain)
+			}
+		}(i)
+	}
+
+	wg.Wait()
 }
